5fab873d007329b70b8774be641c71e7a5e1d327
LAPTOP-G4C6KKPL\sevin <sebas.villanueva.98@gmail.com>
2019-04-10 12:51:50 -0500

    Validaciones TCP y MQTT prueba

--- NetIoTEmulator/src/co/edu/usbbog/datan/niote/modelo/PuertaDeEnlace.java
+++ NetIoTEmulator/src/co/edu/usbbog/datan/niote/modelo/PuertaDeEnlace.java
@@ -5,6 +5,13 @@
  */
 package co.edu.usbbog.datan.niote.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
 /**
@@ -21,6 +28,10 @@ public class PuertaDeEnlace {
     private List<Mensaje> mensajes;    
     private List<Nodo> nodos;
     private List<PuertaDeEnlace> salidas;
+    ServerSocket serversocket;
+    Socket client;
+    BufferedReader input;
+    PrintWriter output;
 
     public PuertaDeEnlace(String id, String descripcion, boolean estado, String direccionLogica, String puertoDeServicio, String protocoloComunicacionExterno) {
         this.id = id;
@@ -29,6 +40,39 @@ public class PuertaDeEnlace {
         this.direccionLogica = direccionLogica;
         this.puertoDeServicio = puertoDeServicio;
         this.protocoloComunicacionExterno = protocoloComunicacionExterno;
+    }
+    public void Conexion() throws IOException {
+        serversocket = new ServerSocket(9090);
+        System.out.println("Connection Starting on port:" + serversocket.getLocalPort());
+        client = serversocket.accept();
+        System.out.println("Waiting for connection from client");
+        try {
+            iniciarSesion();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void iniciarSesion() throws Exception {
+        input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        String username = input.readLine();
+        System.out.println("username" + username);
+        String password = input.readLine();
+        System.out.println("password" + password);
+        output = new PrintWriter(new OutputStreamWriter(client.getOutputStream()));
+        if (username.equals("sevin") && password.equals("sevin")) {
+            output.println("Welcome, " + username);
+
+        } else {
+            output.println("Login Failed");
+        }
+
+        output.flush();
+        output.close();
+        serversocket.close();
+        //Servidor(1000);
+
     }
 
     public String getId() {
@@ -91,9 +135,4 @@ public class PuertaDeEnlace {
     public void setNodos(List<Nodo> nodos) {
         this.nodos = nodos;
     }
-   
-    
-    
-    
-    
 }
