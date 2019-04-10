3444b29960e3f43b30c6a602075da3a913d79187
andres-karoll <andres-karoll@users.noreply.github.com>
2019-04-10 15:39:37 -0500

    Ajuste 13

--- NetIoTEmulator/src/co/edu/usbbog/datan/niote/controlador/logica/ValidacionesSistema.java
+++ NetIoTEmulator/src/co/edu/usbbog/datan/niote/controlador/logica/ValidacionesSistema.java
@@ -40,7 +40,7 @@ public class ValidacionesSistema {
      * @return
      */
     private boolean configuracionMQTT() {
-        /*String topic = "prueba/neiote";
+        String topic = "prueba/neiote";
         int qos = 2;
         String broker = "tcp://localhost:1883";
         String clientId = "prueba";
@@ -49,20 +49,16 @@ public class ValidacionesSistema {
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             String content = "Hola";
-            if (sampleClient.isConnected()) {
-                
-            }else{
-                connOpts.setCleanSession(true);
-                sampleClient.connect(connOpts);
-            }            
+            connOpts.setCleanSession(true);
+            sampleClient.connect(connOpts);      
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
             sampleClient.publish(topic, message);
+            sampleClient.disconnect();
             return true;
         } catch (MqttException me) {
             return false;
-        }*/
-        return true;
+        }
     }
 
     /**
@@ -76,10 +72,12 @@ public class ValidacionesSistema {
             if (serverSocket.getLocalPort() == -1) {
                 return false;
             }
+            serverSocket.close();
             serverSocket = new ServerSocket(9998);
             if (serverSocket.getLocalPort() == -1) {
                 return false;
             }
+            serverSocket.close();
             return true;
 
         } catch (IOException ex) {
