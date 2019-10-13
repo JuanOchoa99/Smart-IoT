b976a9564695268c48696f0d7457ca8939790f2d
Sebastian Villanueva <sebas.villanueva.98@gmail.com>
2019-10-13 00:27:34 -0500

    Cambios

--- PIICOWS/target/generated-sources/annotations/co/edu/usbbog/datan/piico/piicows/model/Rol_.java
+++ PIICOWS/target/generated-sources/annotations/co/edu/usbbog/datan/piico/piicows/model/Rol_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-13T00:03:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-13T00:25:59")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
