74065fabe8a1af0bc5db87d98271387d6283123c
Sebastian Villanueva <sebas.villanueva.98@gmail.com>
2020-05-31 15:44:44 -0500

    Pagina de presentación

---
+++ PIICO_PS/_/galeria.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Sebastián Enrique Villanueva Navarro, Juan José Ochoa Ortiz">
+
+    <title>PIICO - Universidad San Buenaventura</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="img/icono.ico">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.css" rel="stylesheet">
+
+
+</head>
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 5px grey;
+        border-radius: 10px;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #007bff;
+        border-radius: 5px;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #007bff;
+    }
+</style>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary static-top fixed-top">
+        <a class="navbar-brand" href="/">
+            <img src="img/iconoPiico.png" alt="Logo" style="width:50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation" data-target="#navbarPiico">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarPiico">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#semilleros">Semilleros<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#introduccion">Proyecto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#conocenos">Conocenos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="galeria.php">Galeria</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="contactanos.php">Contactanos</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right mr-2">
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <a class="btn btn-warning text-white dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-eye"></i> Demos
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="PIICODS/index3.html" target="blank">CLOUD</a>
+                            <a class="dropdown-item" href="login.html" target="blank">GATEWAY</a>
+                            <a class="dropdown-item" href="PIICODS_ND/estacion.html" target="blank">ESTACÍON</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right mr-2">
+                <li class="nav-item">
+                    <a class="btn text-white btn-success btn-sm" href="login.php"><i class="fas fa-sign-in-alt"></i> Ingresar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <header class="masthead text-white text-center">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto">
+                    <h1 class="text-white">Galería</h1>
+                    <h2 class="text-white">Fotos de la experiencia del desarrollo del proyecto</h2>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="testimonials text-center bg-light" id="galeria">
+
+        <div class="row">
+            <div class="container">
+                <div class="row text-center text-lg-left">
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (3).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (4).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (7).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (2).jpeg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (5).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (8).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (9).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (10).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (2).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto0.jpeg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (6).jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-6">
+                        <a href="#" class="d-block mb-4 h-100">
+                            <img class="img-fluid img-thumbnail" src="FotosPIICO/foto1 (2).png" alt="">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Call to Action -->
+    <!-- Footer -->
+    <footer class="footer bg-primary text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                    <ul class="list-inline mb-2">
+                        <p class=" text-white small mb-4 mb-lg-0">&copy; Universidad de San Buenaventura 2019. Todos los derechos reservados.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ PIICO_PS/_/login.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Sebastián Enrique Villanueva Navarro, Juan José Ochoa Ortiz">
+
+    <title>PIICO - Ingresar Plataforma de Analitica</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="img/icono.ico">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.min.css" rel="stylesheet">
+
+
+</head>
+<style>
+    .main-head {
+        height: 150px;
+        background: #4CA2FE;
+    }
+
+    .sidenav {
+        height: 100%;
+        background-color: #4CA2FE;
+        overflow-x: hidden;
+        padding-top: 20px;
+    }
+
+    .main {
+        padding: 0px 10px;
+    }
+
+    @media screen and (max-height: 450px) {
+        .sidenav {
+            padding-top: 15px;
+        }
+    }
+
+    @media screen and (max-width: 450px) {
+        .login-form {
+            margin-top: 10%;
+        }
+
+        .register-form {
+            margin-top: 10%;
+        }
+    }
+
+    @media screen and (min-width: 768px) {
+        .main {
+            margin-left: 40%;
+        }
+
+        .sidenav {
+            width: 30%;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+        }
+
+        .login-form {
+            margin-top: 80%;
+        }
+
+        .register-form {
+            margin-top: 20%;
+        }
+    }
+
+    .login-main-text {
+        margin-top: 20%;
+        padding: 60px;
+        color: #fff;
+    }
+
+    .login-main-text h2 {
+        font-weight: 300;
+    }
+
+    .btn-black {
+        background-color: #4CA2FE !important;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 5px grey;
+        border-radius: 10px;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #4CA2FE;
+        border-radius: 5px;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #4CA2FE;
+    }
+</style>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary static-top fixed-top">
+        <a class="navbar-brand" href="/">
+            <img src="img/iconoPiico.png" alt="Logo" style="width:50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation" data-target="#navbarPiico">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarPiico">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#semilleros">Semilleros<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#introduccion">Proyecto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="/#conocenos">Conocenos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="galeria.php">Galeria</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn text-white btn-md" href="contactanos.php">Contactanos</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right mr-2">
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <a class="btn btn-warning text-white dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-eye"></i> Demos
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="PIICODS/index3.html" target="blank">CLOUD</a>
+                            <a class="dropdown-item" href="login.html" target="blank">GATEWAY</a>
+                            <a class="dropdown-item" href="PIICODS_ND/estacion.html" target="blank">ESTACÍON</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right mr-2">
+                <li class="nav-item">
+                    <a class="btn text-white btn-success btn-sm" href="login.php"><i class="fas fa-sign-in-alt"></i> Ingresar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="sidenav">
+        <div class="login-main-text text-center">
+            <img src="img/iconoPiico.png" alt="Piico" style="width:150px">
+            <h1>PIICO<br>Ingreso a la Plataforma de Analítica</h1>
+        </div>
+    </div>
+    <div class="main">
+        <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+                <form>
+                    <div class="form-group">
+                        <label>Usuario</label>
+                        <input type="text" class="form-control" placeholder="Usuario">
+                    </div>
+                    <div class="form-group">
+                        <label>Contreseña</label>
+                        <input type="password" class="form-control" placeholder="Contraseña">
+                    </div>
+                    <a type="button" class="btn btn-success" href="../PIICODS/index3.html">Ingresar</a>
+                    <a type="button" class="btn btn-warning text-white" href="registro.php">Registro</a>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
