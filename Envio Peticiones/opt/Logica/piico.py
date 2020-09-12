c8459bcde237cbe5a64e3903be7ecda02659a583
Sebastian Villanueva <sebas.villanueva.98@gmail.com>
2020-09-12 03:19:09 -0500

    Programa de configuración NODO

--- Envio Peticiones/opt/Logica/piico.py
+++ Envio Peticiones/opt/Logica/piico.py
@@ -9,7 +9,11 @@ import json
 import sys
 import paho.mqtt.client as mqtt
 import datetime
+import threading
+import time
 
+hostname = ""
+puerto = 0            
 class Archivo:
     
     def Leer_Archivo(self,nombre,ruta):
@@ -55,6 +59,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe([("conf_l", 2),("req_l", 2),("act_l",2)])
 def on_message(client, userdata, message):
     print('------------------------------')
+    brokerPub = ConexionPub()
+    brokerPubSta = ConexionSta()
+    construir_json = EstadoAct()
     #print('topic: %s', message.topic)
     #print("%s %s" % (message.topic,message.payload))
     topico = message.topic
@@ -120,57 +127,171 @@ def on_message(client, userdata, message):
                                     "value":"0",
                                     "magnitude":"w/m2" })
             #print("-------SEN_l---------\n",json.dumps(nuevo_json['sensors']))
-            topic = "sen_l"
             print("chao")
-            client.publish(topic, json.dumps(nuevo_json))
+            topic = "sen_l"
+            threadPublicador = threading.Thread(name="Publicador", target=brokerPub.publicadorMas, args=(topic, nuevo_json))
+            threadPublicador.start()
+            print(threadPublicador.isAlive())
+            print("chao")
         elif data['request'] == "stop":
-            print("hola2")
-            nuevo_json = {}
-            nuevo_json['node-id']= data['node-id']
-            nuevo_json['date'] = data['date']
-            nuevo_json['sensors'] =[]
-            posicion = 0
-            #print(nuevo_json)
-            for sensor in data['sensors']:
-                    if sensor['sensor-id'] == "Temperature":
-                        nuevo_json['sensors'].append({
-                                    "type":"tem",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0.0",
-                                    "magnitude":"C" })
-                    if sensor['sensor-id'] == "Humidity":
-                        nuevo_json['sensors'].append({
-                                    "type":"hum",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0",
-                                    "magnitude":"%" })
-                    if sensor['sensor-id'] == "Velocity":
-                        nuevo_json['sensors'].append({
-                                    "type":"hum",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0",
-                                    "magnitude":"km/h" })
-                    if sensor['sensor-id'] == "Direction":
-                        nuevo_json['sensors'].append({
-                                    "type":"hum",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0",
-                                    "magnitude":"grados" })
-                    if sensor['sensor-id'] == "Pluviometer":
-                        nuevo_json['sensors'].append({
-                                    "type":"hum",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0",
-                                    "magnitude":"ml" })
-                    if sensor['sensor-id'] == "Radiation":
-                        nuevo_json['sensors'].append({
-                                    "type":"hum",
-                                    "sensor-id":""+sensor['sensor-id']+"",
-                                    "value":"0",
-                                    "magnitude":"w/m2" })
-            topic = "sen_l"
-            print("chao")
-            client.publish(topic, json.dumps(nuevo_json))
+            threadPublicador.stop()
+        elif data['request'] == "info":
+            print("Comienzo")
+            nuevo_json = construir_json.armar_json(data)
+            topic = "sta_l"
+            print("Chao", nuevo_json)
+            print(hostname,puerto)
+            threadPublicadorSta = threading.Thread(name="Publicador", target=brokerPubSta.publicador, args=( topic, nuevo_json))
+            threadPublicadorSta.start()
+    elif topico == "act_l":
+        data = json_decode(message.payload)
+        if data['request'] == "info":
+            nuevo_json = construir_json.armar_json(data)
+            topic = "sta_l"
+            threadPublicadorSta = threading.Thread(name="Publicador", target=brokerPubSta.publicador, args=( topic, nuevo_json))
+            threadPublicadorSta.start()
+        elif data['request'] == "act":
+            if data['actuators']['actuator-id'] == "Aspersor":
+                aspersor = True #Reemplazar por rutina de aspersor
+    
+    
+class EstadoAct:
+    def armar_json(self,data):
+        leer_archivo = Archivo()
+        leer_conf = json.loads(leer_archivo.Leer_Archivo("conf_l.json","C:\\Envio Peticiones\\etc\\piico\\"))
+        nuevo_json = {}
+        nuevo_json['node-id']= data['node-id']
+        nuevo_json['date'] = data['date']
+        nuevo_json['broker'] ={}
+        nuevo_json['broker']['publish'] = []
+        nuevo_json['broker']['suscribe'] = []
+        nuevo_json['interfaces'] = []
+        nuevo_json['sensors'] =[]
+        nuevo_json['actuators'] =[]
+        print(nuevo_json)
+        print(data['information']['mqtt'])
+        print(data['information']['actuators'])
+        print(data['information']['sensors'])
+        print(data['information']['interfaces'])
+        if data['information']['mqtt'] == "true":
+            print("segundo if")
+            nuevo_json['broker']['ip'] = leer_conf['broker']['broker_address']
+            nuevo_json['broker']['port'] = leer_conf['broker']['port']
+            nuevo_json['broker']['qos'] = leer_conf['broker']['qos']
+            nuevo_json['broker']['user'] = leer_conf['broker']['user']
+            nuevo_json['broker']['publish'].append({
+                                "type": "sen_l"})
+            nuevo_json['broker']['publish'].append({
+                                "type": "sta_l"})
+            nuevo_json['broker']['suscribe'].append({
+                                "type": "conf_l"})
+            nuevo_json['broker']['suscribe'].append({
+                                "type": "req_l"})
+            nuevo_json['broker']['suscribe'].append({
+                                "type": "act_l"})
+            print("---------",nuevo_json)
+        if data['information']['interfaces'] == "true":
+            print("tercer if")
+            for interfaz in leer_conf['interfaces']:
+                print(interfaz['type'])
+                print(leer_conf['interfaces'])
+                print()
+                if interfaz['type'] == "wifi":
+                    nuevo_json['interfaces'].append({
+                                "type": "wifi",
+                                "state": "active",
+                                "dir": ""+leer_conf['broker']['broker_address']+"" })
+                if interfaz['type'] == "blue":
+                    print(nuevo_json)
+                    nuevo_json['interfaces'].append({
+                                "type": "blue",
+                                "state": "active",
+                                "mac": ""+interfaz['mac']+"",
+                                "mode": "direct"})
+                    print(nuevo_json)
+                if interfaz['type'] == "xbee":
+                    nuevo_json['interfaces'].append({
+                                "type": "xbee",
+                                "state": "active",
+                                "pan-id": "1234",
+                                "mac": ""+interfaz['mac']+""})
+        print(data['information']['sensors'])
+        if data['information']['sensors'] == "true":
+            print("4 if")
+            for sensor in leer_conf['sensors']:
+                if sensor['type'] == "tem":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Temperature",
+                                "state": "active" })
+                if sensor['type'] == "hum":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Humidity",
+                                "state": "active" })
+                if sensor['type'] == "vel":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Velocity",
+                                "state": "active" })
+                if sensor['type'] == "dir":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Direction",
+                                "state": "active" })
+                if sensor['type'] == "plu":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Pluviometer",
+                                "state": "active" })
+                if sensor['type'] == "rad":
+                    nuevo_json['sensors'].append({
+                                "sensor-id": "Radiation",
+                                "state": "active" })
+        if data['information']['actuators'] == "true":
+            print("5 if")
+            for actuador in leer_conf['actuators']:
+                if actuador['type'] == "asp":
+                    nuevo_json['actuators'].append({
+                                "actuator-id": "Aspersor",
+                                "state": "active"})
+                if actuador['type'] == "led":
+                    nuevo_json['actuators'].append({
+                                "actuator-id": "RGB",
+                                "state": "active"})
+        return nuevo_json
+
+class ConexionMqtt:
+    def sucriptor(self, hostSub, puertoSub):
+        client = mqtt.Client(client_id='Sevin', clean_session=False)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        print("hostname= ",hostSub,"puerto= ",puertoSub)
+        client.connect(host=hostSub, port=puertoSub)
+        #client.loop()
+        client.loop_forever()
+
+class ConexionPub:
+    def publicadorMas(self, topicoPub, mensajePub):
+        leer_archivo = Archivo()
+        json_leer = json.loads(leer_archivo.Leer_Archivo("conf_l.json","C:\\Envio Peticiones\\etc\\piico\\"))
+        hostPub = json_leer['broker']['broker_address']
+        puertoPub = int(json_leer['broker']['port'])
+        while True:
+            service = mqtt.Client('piicoPub') # Creación del cliente?Ė
+            service.connect(host= hostPub, port=puertoPub)
+            topic = topicoPub
+            mensaje = mensajePub
+            service.publish(topic, json.dumps(mensaje))
+            print(json_leer['send-frequency'])
+            time.sleep(json_leer['send-frequency'])
+class ConexionSta:
+    def publicador(self, topicoPubSta, mensajePubSta):
+        leer_archivo = Archivo()
+        json_leer = json.loads(leer_archivo.Leer_Archivo("conf_l.json","C:\\Envio Peticiones\\etc\\piico\\"))
+        hostPubSta = json_leer['broker']['broker_address']
+        puertoPubSta = int(json_leer['broker']['port'])
+        service = mqtt.Client('piicoPub') # Creación del cliente?Ė
+        service.connect(host= hostPubSta, port=puertoPubSta)
+        topic = topicoPubSta
+        mensaje = mensajePubSta
+        print(topic,mensaje,hostPubSta)
+        service.publish(topic, json.dumps(mensaje))
 
     
 def main():     
@@ -190,12 +311,8 @@ def main():
     json_leer = json.loads(leer_archivo.Leer_Archivo("conf_l.json","C:\\Envio Peticiones\\etc\\piico\\"))
     hostname = json_leer['broker']['broker_address']
     puerto = int(json_leer['broker']['port'])
-    client = mqtt.Client(client_id='Sevin', clean_session=False)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    print("hostname= ",hostname,"puerto= ",puerto)
-    client.connect(host=hostname, port=puerto)
-    #client.loop()
-    client.loop_forever()
+    broker = ConexionMqtt()
+    threadSuscriptor = threading.Thread(name="Suscriptor", target=broker.sucriptor, args=(hostname, puerto))
+    threadSuscriptor.start()
 if  __name__ ==  '__main__':
     main()
