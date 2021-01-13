2e8617546a0a98a5c8e6c63b24f89f6337fc6fdd
JuanOchoa99 <38961797+JuanOchoa99@users.noreply.github.com>
2021-01-13 02:18:13 -0500

    actualizacion mapa

--- piico_ws/src/main/java/co/edu/usbbog/piico/piicows/service/SensorService.java
+++ piico_ws/src/main/java/co/edu/usbbog/piico/piicows/service/SensorService.java
@@ -567,7 +567,7 @@ public class SensorService implements ISensorService{
 				json.put("title", estacion);
 				json.put("latitude", lat);
 				json.put("longitude", lon);
-				json.put("svgPath", "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S1…,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z");
+				json.put("svgPath", "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z");
 				json.put("zoomLevel", 5);
 				json.put("scale", 0.5);
 				dia = hd.getDate();
@@ -581,7 +581,7 @@ public class SensorService implements ISensorService{
 		json.put("title", estacion);
 		json.put("latitude", lat);
 		json.put("longitude", lon);
-		json.put("svgPath", "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S1…,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z");
+		json.put("svgPath", "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z");
 		json.put("zoomLevel", 5);
 		json.put("scale", 0.5);
 		System.out.println("Json Resultado: "+json);
